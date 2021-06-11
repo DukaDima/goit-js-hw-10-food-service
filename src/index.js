8bd2b737f6f01d4bc54fdab27af02e12c0f34065
Dima <75967292+DukaDima@users.noreply.github.com>
2021-06-11 18:14:33 +0300

    Переключает темы

--- src/index.js
+++ src/index.js
@@ -5,22 +5,20 @@ import menuCardTmpl from './templates/menu-card.hbs'
 import menuCards from './menu.json'
 
 // массив ссылок----------------------------------------------
+
 const refs = {
     body: document.body,
     menu: document.querySelector('.js-menu'),
-    checkBox: document.querySelector('#theme-switch-toggle')
-   
+    checkBox: document.querySelector('#theme-switch-toggle') 
 }
-// console.log(refs.checkBox)
-console.log(refs.body)
+
 //массив тем -------------------------------------------------
 
 const Theme = {
   LIGHT: 'light-theme',
   DARK: 'dark-theme',
 };
-// console.log(Theme.LIGHT)
-// console.log(Theme.DARK)
+
 //создание меню ---------------------------------------------
 
 const menuList = createMenuCards(menuCards)
@@ -29,17 +27,29 @@ refs.menu.insertAdjacentHTML('beforeend',menuList)
 function createMenuCards(menuCards) {
     return menuCards.map(menuCardTmpl).join('');
 };
+
 // переключение тем------------------------------------------
 
-
 refs.checkBox.addEventListener('change', onChange)
-refs.body.classList.add("light-theme")
+
+if (!localStorage.getItem('thema')) {
+  refs.body.classList.add(Theme.LIGHT)
+  localStorage.setItem('thema', refs.body.classList)
+}
+
+refs.body.className = localStorage.getItem('thema')
 
 function onChange() {
-
-  // console.log("Состояние чекбокса изменнено")
   
-  refs.body.classList.toggle("dark-theme")
-  refs.body.classList.toggle("light-theme")
+  refs.body.classList.toggle(Theme.DARK)
+  refs.body.classList.toggle(Theme.LIGHT)
 
+   return localStorage.setItem('thema',refs.body.classList)
 }
+// проверка кнопки на checked----------------------
+const currentClass = localStorage.getItem('thema')
+if (currentClass === Theme.DARK) {
+  refs.checkBox.setAttribute("checked", true)
+} else {
+  refs.checkBox.removeAttribute("checked")
+};
